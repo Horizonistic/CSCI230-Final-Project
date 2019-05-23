284919d16cba4ea50c692d418eb31c2612c33aa4
Cameron Grigoriadis <camgrigoriadis@gmail.com>
2019-05-23 16:01:11 -0700

    Add Game

---
+++ src/csci/pkg230/pkgfinal/project/Game.java
@@ -0,0 +1,34 @@
+package csci.pkg230.pkgfinal.project;
+
+
+// Represents game scoring and mechanics.
+public class Game {
+
+    // Current score.
+    private int score = 0;
+
+    private int pointValue = 1;
+
+    // Constructor.
+    public Game() {
+    }
+
+    // Increment
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
+
+    // Score
+    public void addPoint() {
+        score += pointValue;
+    }
+
+    public void setScore(int newScore) {
+        score = newScore;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+}
